d05c94b917efe47192b137eae171ef8c26e2a02b
Michael Blatherwick <michael@wildknowledge.co.uk>
2012-12-06 16:36:39 +0000

    Fix package install
    Use setuptools.find_packages() and a MANIFEST.in to
    include everything properly upon setup.

--- setup.py
+++ setup.py
@@ -1,24 +1,22 @@
 import os
 #from setuptools import setup
 from distutils.core import setup
+from setuptools import find_packages
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='django-feedback',
-    version='0.4',
+    version='0.5',
     description='A pluggable user feedback app',
     author='Jared Forsyth',
     author_email='jabapyth@gmail.com',
     license='BSD',
-    url='http://github.com/unaizalakain/django-feedback/',
+    url='http://github.com/craigloftus/django-feedback/',
     keywords = ['blog', 'django', 'feedback', 'ajax', 'user', 'customer', 'comment'],
-    package_data = {'feedback': ['static/feedback/*', 'static/feedback/images/*', 'CREDITS']},
-    install_requires=['yammy'],
-    packages=[
-        'feedback',
-    ],
+    packages = find_packages(),
+    include_package_data=True,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
