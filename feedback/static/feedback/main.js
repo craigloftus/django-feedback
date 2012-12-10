b67daceb2856c0c3399490a538e4acf06092d6c3
Michael Blatherwick <michael@wildknowledge.co.uk>
2012-12-10 12:45:01 +0000

    Tell user if feedback could not be sent

--- feedback/static/feedback/main.js
+++ feedback/static/feedback/main.js
@@ -9,14 +9,22 @@ feedback.init = function(config) {
         'onend':function(){
             config.modal.find('.btn-primary').removeClass('loading');
         },
+        'onerror':function(){
+            var error_message = config.modal.find('.alert-error');
+            error_message.show();
+        },
         'onsuccess':function(){
             var success_message = config.modal.find('.alert-success');
             success_message.show();
             setTimeout(function(){
                 config.modal.modal('hide');
-                success_message.hide();
                 config.modal.find('textarea, input').not(':hidden').val('');
             }, 2000);
         }
+    });
+
+    // Hide alert messages when the modal gets hidden
+    config.modal.on('hide', function () {
+        config.modal.find('.alert').hide();
     });
 };
